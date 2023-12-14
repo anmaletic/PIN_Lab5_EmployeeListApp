5ddc55073d9b7a79b29d7daa367941081765ac28
AnMal <junkie_@msn.com>
2023-12-14 18:17:52 +0100

    Add project files.

---
+++ EmployeeListApp/Data/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeListApp.Data
+{
+    public class Employee
+    {
+        public string? Id { get; set; }
+        public string? FulLName { get; set; }
+        public string? Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeListApp.Data;
+
+namespace EmployeeListApp.Services;
+
+public class EmployeesService
+{
+    private readonly AppDbContext _context;
+
+    public EmployeesService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Employee>> GetEmployeesAsync()
+    {
+        var result = _context.Employees;
+        return await Task.FromResult(result.ToList());
+    }
+
+    public async Task<Employee?> GetEmployeeByIdAsync(string id)
+    {
+        return await _context.Employees.FindAsync(id);
+    }
+
+    public async Task<Employee> InsertEmployeeAsync(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+        return employee;
+    }
+
+    public async Task<Employee?> UpdateEmployeeAsync(string id, Employee e)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee == null)
+            return null;
+
+        employee.FulLName = e.FulLName;
+        employee.Department = e.Department;
+        employee.Salary = e.Salary;
+
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+        return employee;
+    }
+
+    public async Task<Employee?> DeleteEmployeeAsync(string id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee == null)
+            return null;
+        _context.Employees.Remove(employee);
+        await _context.SaveChangesAsync();
+        return employee;
+    }
+
+}
